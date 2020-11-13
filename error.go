aa70fa979e3cd195688565ee2b9547cf36ca338d
v_jhhlu(陆建辉) <v_jhhlu@tencent.com>
2020-11-13 20:17:22 +0800

    update

---
+++ error.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	//"errors"
+	"fmt"
+)
+//type error interface {
+//	Error() string
+//}
+//
+//
+//func Sqrt(f float64)(float64, error){
+//	if f < 0{
+//		return 0, errors.New("math: square root of negative number")
+//	}
+//
+//	return 1, nil
+//}
+
+
+func main(){
+	//_, err := Sqrt(-1)
+	//if err != nil{
+	//	fmt.Println(err)
+	//}
+
+	if result, errorMsg := Divide(100, 10); errorMsg == ""{
+		fmt.Println("100/10 = ", result)
+	}
+	if _, errMsg := Divide(100, 0); errMsg != ""{
+		fmt.Println("errorMsg is: ", errMsg)
+	}
+}
+
+type DivideError struct {
+	dividee int
+	divider int
+}
+
+func (de *DivideError) Error() string{
+	strFormat := `
+	Cannot proceed, the divider is zero.
+	divedee: %d
+	diveder: 0
+`
+	return fmt.Sprintf(strFormat, de.dividee)
+}
+
+func Divide(varDividee int, varDivider int)(result int, errorMsg string){
+	if varDivider == 0{
+		dData := DivideError{
+			dividee: varDividee,
+			divider: varDivider,
+		}
+		errorMsg = dData.Error()
+		return
+	} else {
+		return varDividee / varDivider, ""
+	}
+}
---
+++ interface.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+type Phone interface{
+	call()
+}
+
+
+type HWPhone struct{
+}
+
+func (hwPhone HWPhone) call() {
+	fmt.Println("I am HW, I can call you!")
+}
+
+type IPhone struct {
+}
+
+func (iphone IPhone) call(){
+	fmt.Println("I am iphone, I can call you!")
+}
+
+
+func main(){
+	var phone Phone
+	phone = new(HWPhone)
+	phone.call()
+
+	phone = new(IPhone)
+	phone.call()
+
+}
